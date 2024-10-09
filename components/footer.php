7b4ae4e156c5e8a56e80c7fa467c2fa4141fb43d
Hasindu Bimalka <hasindubimalka2002@gmail.com>
2024-10-09 21:47:52 +0530

    first commit

---
+++ components/footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>Our Email</h3>
+         <a href="mailto:hasindubimalka09@gmail.com">hasindubimalka09@gmail.com</a>
+         <a href="mailto:bimalkahasindu4@gmail.com">bimalkahasindu4@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>Opening Hours</h3>
+         <p>08:00am to 08:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>Our Address</h3>
+         <a href="#">Rambukkana, Kegalle</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>Our Number</h3>
+         <a href="tel:0772547128">+94 772547128</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Hasindu_HB</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
